56525a56d6952eadd6f66f0bdab689158ca8a57e
Mahpara <mah4.para@gmail.com>
2025-10-26 13:20:37 +0000

    chore: testing

--- src/auth.js
+++ src/auth.js
@@ -8,8 +8,8 @@ export const { auth, handlers, signOut, signIn } = NextAuth({
   secret: process.env.AUTH_SECRET,
   providers: [
     GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
   ],
   trustHost: true,
